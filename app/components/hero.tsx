c7752201bca2e53e8713a278a8490220eddbe511
Cherkoz <cherkozyanovvladislav@mail.ru>
2025-11-24 23:49:44 +0300

    update hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export function Hero() {
     return (
-        <div className="container flex justify-center flex-col items-center gap-4">
-            <div className="flex items-center">
-                <h1 className="text-4xl">Владислав + Валерия =</h1>
-                <Image src="" alt="" className="w-10 h-10 bg-red-500" />
+        <div className="container overflow-hidden min-h-screen flex justify-center flex-col items-center gap-1">
+            <h1 className="leading-[0.9] flex flex-col text-4xl md:text-6xl text-brown text-center mb-2 md:block">
+                Владислав <span>+</span> Валерия
+            </h1>
+
+            <div className="relative flex flex-col items-center bg-white p-1 shadow w-fit">
+                <Image src="/assets/images/hero-pink.png" className="-z-10 absolute -top-26 w-[803px] min-w-[803px] h-[540px]" width={803} height={540} alt="" />
+                <Image src="/assets/images/love.jpg" width={747} height={1024} alt="" className="w-50 h-70" />
+                <div className="font-anastasia text-4xl mt-2 -ml-3">Мы женимся!</div>
             </div>
 
-            <div className="bg-white p-1 shadow w-fit">
-                <Image src="" alt="" className="w-50 h-70 bg-red-500" />
-                <div className="font-anastasia text-4xl mt-2">Мы женимся!</div>
-            </div>
-
-            <div className="text-center">
+            <div className="text-center font-anastasia text-4xl leading-[0.9] -ml-3">
                 <div>27.06.2026</div>
                 <div>17:00</div>
             </div>
