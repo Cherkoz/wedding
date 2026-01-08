945ffe1c68f75b74833e17c2939562ad899e0c9d
Cherkoz <cherkozyanovvladislav@mail.ru>
2026-01-08 21:12:52 +0300

    add CiHeart

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,3 +1,6 @@
+import { BsHeartFill } from "react-icons/bs";
+import { CiHeart } from "react-icons/ci";
+
 export function Calendar() {
     const textStyle = 'text-center py-2';
 
@@ -7,7 +10,7 @@ export function Calendar() {
                 Дата бракосочетания
             </h2>
 
-            <p className="text-center mb-2">Июнь</p>
+            <p className="text-center font-extrabold text-xl mb-2">Июнь</p>
             <div className="w-full grid grid-cols-7 gap-2">
                 <div className={textStyle}>пн</div>
                 <div className={textStyle}>вт</div>
@@ -20,8 +23,9 @@ export function Calendar() {
                 {Array.from({ length: 30 }, (_, i) => i + 1).map((num) => (
                     <div
                         key={num}
-                        className={`text-center py-2 ${num === 27 ? 'text-red-500' : ''}`}
+                        className={`relative flex justify-center items-center text-center py-2 ${num === 27 ? 'text-red-500' : ''}`}
                     >
+                        {num === 27 && <CiHeart className="absolute mt-1 w-18 h-18" />}
                         {num}
                     </div>
                 ))}
--- app/components/details.tsx
+++ app/components/details.tsx
@@ -9,20 +9,20 @@ export function Details() {
                 <Image src="/assets/images/details.jpeg" width={1000} height={1000} alt="" className="w-full" />
                 <div>
                     <div className="border-b border-amber-950 pb-2 mb-4">
-                        <h3 className="text-xl">Подарки</h3>
+                        <h3 className="text-xl font-extrabold">Подарки</h3>
                         <p>Ваше присутствие - лучший подарок, но если вы захотите сделать еще что-то, будем рады конвертику</p>
                     </div>
 
                     <div className="border-b border-amber-950 pb-2 mb-4">
-                        <h3 className="text-xl">Контакты</h3>
+                        <h3 className="text-xl font-extrabold">Контакты</h3>
                         <p>
                             По возникающим вопросам можете обращаться по номеру телефона невесты <br/>
-                            +7 (977) 415 96 46
+                            <a className="font-extrabold">+7 (977) 415 96 46</a>
                         </p>
                     </div>
 
                     <div>
-                        <h3 className="text-xl">Чат для гостей</h3>
+                        <h3 className="text-xl font-extrabold">Чат для гостей</h3>
                         <p>Мы создали группу в Telegram. куда можно будет добавлять фото и видео с нашей свадьбы, чтобы сохранить в памяти лучшие моменты</p>
                         <button className="py-4 px-6 border border-orange-950 rounded-2xl mt-6">Вступить в чат</button>
                     </div>
