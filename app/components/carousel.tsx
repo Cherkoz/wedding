b6e5e5f0a507186b876fe07bdcb19f0b9a59490c
Cherkoz <cherkozyanovvladislav@mail.ru>
2026-01-09 22:51:15 +0300

    update carousel

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -29,7 +29,7 @@ export function Carousel({
       <div className="dark:from-background dark:to-background/0 absolute top-0 left-0 z-50 h-full w-4 bg-gradient-to-r from-white to-white/0 md:w-10 xl:w-[calc((100%-1280px)/2)]"></div>
       <div className="dark:from-background dark:to-background/0 absolute top-0 right-0 z-50 h-full w-4 bg-gradient-to-l from-white to-white/0 md:w-10 xl:w-[calc((100%-1280px)/2)]"></div>
 
-      <div className="container">
+      <div className="container py-0!">
         <Swiper
           modules={[Pagination]}
           spaceBetween={spaceBetween}
--- app/components/location.tsx
+++ app/components/location.tsx
@@ -2,10 +2,10 @@ import { CarouselLocation } from "./carousel-location";
 
 export function Location() {
     return (
-        <div className="flex flex-col items-center gap-6 w-full relative max-w-screen overflow-x-hidden">
+        <div className="container flex flex-col items-center gap-6 w-full relative max-w-screen! overflow-x-hidden">
             <h2 className="font-anastasia text-6xl text-center">Локация</h2>
             <p className="text-center">
-                Ресторан Catch,<br />
+                <span className="font-extrabold text-xl">Ресторан Catch,</span><br />
                 ул. Академика Пилюгина, 3, Москва
             </p>
             <CarouselLocation />
@@ -15,7 +15,6 @@ export function Location() {
             <a
                 href="https://yandex.ru/maps/213/moscow/?ll=37.530430%2C55.670252&mode=poi&poi%5Bpoint%5D=37.528808%2C55.671122&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D36700959360%26yclid%3D16656974419604799487&z=17.23"
                 target="_blanck"
-                className="mt-6"
             >
             <button className="py-4 mx-auto px-6 bg-pink-100 rounded-2xl">Посмотреть на карте</button>
             </a>
