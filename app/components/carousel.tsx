80e251b1ee7d554bcf1cf72688920aa0d69e1e90
Cherkoz <cherkozyanovvladislav@mail.ru>
2026-01-11 18:01:43 +0300

    слайдэ

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -30,9 +30,8 @@ export function Carousel({
       <div className="dark:from-background dark:to-background/0 absolute top-0 left-0 z-50 h-full w-4 bg-gradient-to-r from-white to-white/0 md:w-10 xl:w-[calc((100%-1280px)/2)]"></div>
       <div className="dark:from-background dark:to-background/0 absolute top-0 right-0 z-50 h-full w-4 bg-gradient-to-l from-white to-white/0 md:w-10 xl:w-[calc((100%-1280px)/2)]"></div>
 
-      <div className="container py-0!">
+      <div className="max-w-screen md:container px-6! md:py-0!">
         <Swiper
-          initialSlide={1}
           modules={[Pagination, Navigation]}
           spaceBetween={spaceBetween}
           slidesPerView={1}
