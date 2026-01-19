be5282e1bbf648f0d50eb5102852e181abe991c3
Cherkoz <cherkozyanovvladislav@mail.ru>
2026-01-19 22:01:21 +0300

    test hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -13,9 +13,9 @@ export function Hero() {
             </h1>
 
             <div className="relative flex flex-col items-center bg-white p-1 shadow w-fit">
-                <Image src="/assets/images/hero-pink.png" className="-z-10 absolute -top-26 w-[803px] min-w-[803px] h-[540px]" width={803} height={540} alt="" />
+                {/* <Image src="/assets/images/hero-pink.png" className="-z-10 absolute -top-26 w-[803px] min-w-[803px] h-[540px]" width={803} height={540} alt="" /> */}
                 {/* Фото — показывается всегда как фон */}
-                {gifLoaded && (
+                {/* {gifLoaded && (
                     <Image
                         width={747}
                         height={1024}
@@ -24,15 +24,15 @@ export function Hero() {
                         className="w-50 h-70"
                         priority
                     />
-                )}
-                {!gifLoaded && (
+                )} */}
+                {/* {!gifLoaded && ( */}
                     <img
                         src="/assets/gifs/love.gif"
                         className="w-50 h-70"
                         alt=""
-                        onLoad={() => setGifLoaded(true)}
+                        // onLoad={() => setGifLoaded(true)}
                     /> 
-                )}
+                {/* )} */}
                 <div className="font-anastasia text-4xl mt-2 -ml-3">Мы женимся!</div>
             </div>
 
